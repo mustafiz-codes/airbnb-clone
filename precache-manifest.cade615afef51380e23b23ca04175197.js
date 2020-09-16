5e9870437247e4de4c4bd9489277056b63a1336d
Mustafiz <js.mustafiz@gmail.com>
2020-09-16 19:53:28 +0600

    Updates

---
+++ precache-manifest.cade615afef51380e23b23ca04175197.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8333cb43671032370e4b62d3a516aab",
+    "url": "/airbnb-clone/index.html"
+  },
+  {
+    "revision": "abd0db7a375fcd610f4e",
+    "url": "/airbnb-clone/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "e7f20187beaa2e01c00a",
+    "url": "/airbnb-clone/static/css/main.a1c1990c.chunk.css"
+  },
+  {
+    "revision": "abd0db7a375fcd610f4e",
+    "url": "/airbnb-clone/static/js/2.c3af7e01.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/airbnb-clone/static/js/2.c3af7e01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7f20187beaa2e01c00a",
+    "url": "/airbnb-clone/static/js/main.4de7c198.chunk.js"
+  },
+  {
+    "revision": "242e0bf819afafd8e77b",
+    "url": "/airbnb-clone/static/js/runtime-main.9e790b1e.js"
+  }
+]);
